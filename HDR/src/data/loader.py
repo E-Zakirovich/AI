db65e898288f4af2efbb587599114899a6951f73
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-05-18 15:00:37 +0500

    update to loader.py file, I initialized core codebase structure. just methods and thats it

---
+++ HDR/src/data/loader.py
@@ -0,0 +1,16 @@
+from .config import TEST_IMAGES, TEST_LABELS, TRAIN_IMAGES, TRAIN_LABELS
+import numpy as np
+import struct 
+
+class Loader:
+	def load_images(self, filepath):
+		pass
+	
+	def load_labels(self, filapath):
+		pass
+		
+	def train_data(self):
+		pass
+		
+	def test_data(self):
+		pass
