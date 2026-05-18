e927f671e9cc3bfb3b3ccfa4bb7345e4f08692b6
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-05-18 14:57:27 +0500

    i created variable to save directories (test & train, labels & images)

---
+++ HDR/config.py
@@ -0,0 +1,8 @@
+# dataset directories
+
+TEST_IMAGES = "data/t10k-images.idx3-ubyte"
+TEST_LABELS = "data/t10k-labels.idx1-ubyte"
+
+TRAIN_IMAGES = "data/train-images.idx3-ubyte"
+TRAIN_LABELS = "data/train-labels.idx1-ubyte"
+
