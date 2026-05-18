647d6a541b7fe9ee582cbb3b0512ff50f52518b2
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-05-18 15:55:58 +0500

    now I can import the data withot problem. Next step is preprocessing part

--- HDR/main.py
+++ HDR/main.py
@@ -2,4 +2,43 @@ from src.data import Loader
 
 loader = Loader()
 
+
 images, labels = loader.train_data()
+
+current = images[35343]
+# print(labels[3])
+
+counter=0
+line = ""
+while counter < 744:
+	if current[counter] == 0:
+		line+=" "
+	elif current[counter] > 0 and current[counter] <128:
+		line+="*"
+	else:
+		line+="#"
+	if counter%28==0:
+		print(line)
+		line=""
+	counter+=1
+	
+	
+
+for r in range(40):
+	current = images[r * 134]
+# print(labels[3])
+
+	counter=0
+	line = ""
+	while counter < 744:
+		if current[counter] == 0:
+			line+=" "
+		elif current[counter] > 0 and current[counter] <128:
+			line+="*"
+		else:
+			line+="#"
+		if counter%28==0:
+			print(line)
+			line=""
+		counter+=1
+		
