fe62d9fec0d6808db3b1b46d86b03cc631f81330
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-05-18 15:49:31 +0500

    image importing process is done

---
+++ HDR/main.py
@@ -0,0 +1,5 @@
+from src.data import Loader
+
+loader = Loader()
+
+images, labels = loader.train_data()
--- HDR/src/data/loader.py
+++ HDR/src/data/loader.py
@@ -14,26 +14,30 @@ the same time. test_data() is same as train_data()
 
 
 
-from .config import TEST_IMAGES, TEST_LABELS, TRAIN_IMAGES, TRAIN_LABELS
+from config import TEST_IMAGES, TEST_LABELS, TRAIN_IMAGES, TRAIN_LABELS
 import numpy as np
 import struct 
 
 class Loader:
 	def load_images(self, filepath):
-		with open(filepath, "rb") as f:
+		with open(filepath, 'rb') as f:
 			magic, num, row, col = struct.unpack(">IIII", f.read(16))
 			images = np.frombuffer(f.read(), dtype=np.uint8)
-            images = images.reshape(num, rows * cols)
-        return images
+			images = images.reshape(num, row * col)
+		return images
 	
 	def load_labels(self, filepath):
-		with open(filepath, "rb") as f:
+		with open(filepath, 'rb') as f:
 			magic, num = struct.unpack('>II', f.read(8))
-            labels = np.frombuffer(f.read(), dtype=np.uint8)
-        return labels
+			labels = np.frombuffer(f.read(), dtype=np.uint8)
+		return labels
 		
 	def train_data(self):
-		pass
+		images = self.load_images(TRAIN_IMAGES)
+		labels = self.load_labels(TRAIN_LABELS)
+		return images, labels
 		
 	def test_data(self):
-		pass
+		images = self.load_images(TEST_IMAGES)
+		labels = self.load_labels(TEST_LABELS)
+		return images, labels
