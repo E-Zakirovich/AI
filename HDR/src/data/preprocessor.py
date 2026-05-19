9c25127d9a3394f9a9c8a718533a3175f0c2a34a
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-05-19 14:19:44 +0500

    finally data pipeline is finished

--- HDR/src/data/preprocessor.py
+++ HDR/src/data/preprocessor.py
@@ -1,3 +1,22 @@
+"""
+preprocessor.py
+~~~~~~~~~~~~~~~
+
+This folder is going to help me to encode MNIST dataset. There is four 
+methods created. They are:
+	
+	normalize(images) - help you to squeeze values from 0 - 255 to 0 - 1
+	
+	one_hot_encode(labels) - help you to encode labels from just number to array form. 
+	example 5 -> [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+	
+	train_data_loader() - it will use normalize(images) and one_hot_encode(labels) methods 
+	then it will return images and labels at the same time. 
+	
+	test_data_loader() - it works same as train_data_loader() method. 
+"""
+
+
 from config import MAX_GREYSCALE, NUM_CLASSES
 import numpy as np
 from .loader import Loader
@@ -17,13 +36,13 @@ class Processing:
 	def train_data_loader(self):
 		images, labels = _loader.train_data()
 		encoded_images = self.normalize(images)
-		encoded_labele = self.one_hot_encode(labels)
-		return encoded_images, encoded_labele
+		encoded_labels = self.one_hot_encode(labels)
+		return encoded_images, encoded_labels
 		
 	def test_data_loader(self):
 		images, labels = _loader.test_data()
 		encoded_images = self.normalize(images)
-		encoded_labele = self.one_hot_encode(labels)
+		encoded_labels = self.one_hot_encode(labels)
 		return encoded_images, encoded_labele
 		
 _processing = Processing()
