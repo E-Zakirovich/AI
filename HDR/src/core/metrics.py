dfab5740c6eb0ad714cd1b654f76ea03a0b641ac
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-05-19 23:26:29 +0500

    I initalized metrics.py file. I wrote comment about all methods, class and main reasons. Also, methods already created, but did not completed yet.

---
+++ HDR/src/core/metrics.py
@@ -0,0 +1,17 @@
+"""
+metrics.py
+~~~~~~~~~~~
+
+I want to know how much my model is predicting  correctly in percentages. To get the 
+result like - "my model is predicting about 96% correct" and so on. Thats why I need 
+metrics.py file. 
+
+I created a class and name it Metrics. Inside of this class there is only one method.
+ It is accuracy(y_pred, y_actual) and it will calculate percentage
+"""
+
+import numpy as np
+
+class Metrics:
+	def accuracy(y_pred, y_actual):
+		pass
